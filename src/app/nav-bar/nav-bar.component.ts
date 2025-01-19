161dbb983052547c264cdcfcc032695863db46ae
Shubham Kumar <shubhammisra800@gmail.com>
2025-01-19 15:23:26 +0530

    update more

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+ windowWid:number
+ visible:boolean=true
+  constructor(){
+    this.windowWid=window.innerWidth
 
+    console.log(this.windowWid)
+    if(this.windowWid<=600){
+      this.visible=false
+    }
+  }
+  ngOnInit(): void {
+    this.windowWid=window.innerWidth
+
+    console.log(this.windowWid)
+    if(this.windowWid<=600){
+      this.visible=false
+    }
+  }
+  menu(){
+    this.visible=!this.visible
+  }
 }
