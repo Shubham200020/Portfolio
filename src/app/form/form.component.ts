b051d533d79d18420cc874a8524c1e3b4539a603
Shubham Kumar <shubhammisra800@gmail.com>
2025-01-23 01:22:49 +0530

    update more data

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,37 +12,48 @@ import * as XLSX from 'xlsx';
 })
 export class FormComponent {
   dsp:boolean=false
-  constructor(private fb: FormBuilder, private http: HttpClient){
+  data:any=[]
+  constructor( private http: HttpClient){
+    this.getData()
   }
   form:FormGroup=new FormGroup({
+    id:new FormControl(null),
     name:new FormControl(''),
     email:new FormControl(''),
     phone:new FormControl(''),
     subject:new FormControl(''),
     contant:new FormControl('')
   })
+  getData(){
+    this.http.get("https://profilebalckend-production.up.railway.app/data").subscribe(
+      (data)=>{
+        this.data=data
+        alert("Hello")
+        //alert("Submit Data Successfully")
+
+      },
+      (error)=>{
+        console.log(error)
+       // this.dsp=true
+
+      }
+    )
+  }
+  
   submit(fmdata:FormGroup){
-    this.dsp=true
-    const formData=fmdata.value;
-    
-    const data = [
-      ['Name', 'Email', 'Phone','subject','Feedback'], // Header row
-      [formData.name, formData.email, formData.phone,formData.subject,formData.contant] // Data row
+   if(fmdata.valid){
+    this.http.post("https://profilebalckend-production.up.railway.app/data/insert",fmdata.value).subscribe(
+      (data)=>{
+        alert("Submit Data Successfully")
 
-    ];
-    const worksheet = XLSX.utils.aoa_to_sheet(data);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+      },
+      (error)=>{
+        console.log(error)
+        alert("issue")
+        this.dsp=true
 
-    // Generate a Blob object from the workbook
-    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    const blob = new Blob([new Uint8Array(excelBuffer)], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-    // Create a download link and trigger the download
-    // const link = document.createElement('a');
-    // link.href = window.URL.createObjectURL(blob);
-    // link.download = 'form_data.xlsx';
-    // link.click();
-
-   alert("Issue") 
+      }
+    )
+   }
   }
 }
