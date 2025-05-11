7bdfe72bba6bd654768868d7197fdca3264847f9
Shubham Kumar <shubhammisra800@gmail.com>
2025-05-11 11:56:22 +0530

    update about resume

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
-
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [PdfViewerModule,CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  pdfDec:boolean=false
   email="shubhammisra800@gmail.com"
   openInNewTab(url:any){
-    // window.open("www.google.com",'_blank')
-    
+    // window.open("www.google.com",'_blank') 
     // window.open("https://www.google.com")
     window.open(url)
-   
+  }
+  show(){
+    this.pdfDec=true
+  }
+  cross(){
+    this.pdfDec=false
   }
 }
