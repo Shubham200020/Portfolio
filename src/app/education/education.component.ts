01c5dbf0c534013ca12384787b6ce26fed781cc5
Shubham Kumar <shubhammisra800@gmail.com>
2026-04-10 15:26:20 +0530

    update some details

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+education = [
+  {
+    year: "2016 - 2017",
+    title: "Higher Secondary (X)",
+    place: "Saraswati Vidya Mandir, Bihar",
+    extra: "CGPA: 7.0"
+  },
+  {
+    year: "2017 - 2020",
+    title: "Senior Secondary (XII)",
+    place: "PC College, Bihar",
+    extra: "Percentage: 60%"
+  },
+  {
+    year: "2020 - 2023",
+    title: "Bachelor's Degree",
+    place: "Pune, MH",
+    extra: "Percentage: 60%"
+  },
+  {
+    year: "2025",
+    title: "MSc Computer Science",
+    univercity: "Indira University",
+    place: "Pune, MH",
+    extra: ""
+  }
+];
 
+experience = [
+  {
+    year: "Feb 2024 - Nov 2024",
+    company: "SetTribe",
+    role: "Intern"
+  }
+];
 }
