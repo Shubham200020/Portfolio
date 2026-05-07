95eca123a320c8a66f042c74b5a9192d2a3d4544
Shubham Kumar <shubhammisra800@gmail.com>
2026-05-07 21:15:02 +0530

    remove cv

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [PdfViewerModule,CommonModule],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  pdfDec:boolean=false
-  email="shubhammisra800@gmail.com"
-  openInNewTab(url:any){
-    // window.open("www.google.com",'_blank') 
-    // window.open("https://www.google.com")
-    window.open(url)
-  }
-  show(){
-    this.pdfDec=true
-  }
-  cross(){
-    this.pdfDec=false
+  email = "shubhammisra800@gmail.com";
+
+  openInNewTab(url: any) {
+    window.open(url);
   }
 }
